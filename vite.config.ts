38cc3492097ea9ff8d5dcf86b98a95ec36d043a9
leonardo0rtega <merortegaleonardo@gmail.com>
2026-08-16 23:56:21 -0600

    metatags OG dinamicos

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { site } from './src/config/site'
+
+function htmlMetadata() {
+  return {
+    name: 'html-metadata',
+    transformIndexHtml(html) {
+      return html
+        .replace(/@@SITE_URL@@/g, site.url)
+        .replace(/@@OG_IMAGE@@/g, site.ogImage)
+    },
+  }
+}
 
 export default defineConfig({
   base: '/LeonardoOrtega/',
   build: {
     outDir: 'docs',
   },
-  plugins: [react()],
+  plugins: [react(), htmlMetadata()],
 })
