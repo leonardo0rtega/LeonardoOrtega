f3174fce01b4fb536d192aff29804e12ced07c4b
leonardo0rtega <merortegaleonardo@gmail.com>
2026-08-16 22:59:40 -0600

    deploy leonardo0rtega

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/leo-portafolio/',
+  base: '/LeonardoOrtega/',
   build: {
     outDir: 'docs',
   },
