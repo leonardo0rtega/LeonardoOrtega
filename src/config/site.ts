93dec5160504f2a804f4c0dd8a6c91c429cd2410
juxn_rx <al222410888@gmail.com>
2026-08-16 23:23:44 -0600

    favicon rel icon v6

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,8 +1,8 @@
 export const site = {
-  url: 'https://leonardo0rtega.github.io/LeonardoOrtega/',
+  url: 'https://juanromero-dotcom.github.io/leo-portafolio/',
   name: 'Leonardo Emmauel Ortega Nava',
   tagline: 'Marketing Strategy & Project Management',
-  photo: 'https://leonardo0rtega.github.io/LeonardoOrtega/images/Leo.jpeg',
-  cv: 'https://leonardo0rtega.github.io/LeonardoOrtega/cv/CV-1.png',
-  ogImage: 'https://leonardo0rtega.github.io/LeonardoOrtega/images/Leo.jpeg',
+  photo: 'https://juanromero-dotcom.github.io/leo-portafolio/images/Leo.jpeg',
+  cv: 'https://juanromero-dotcom.github.io/leo-portafolio/cv/CV-1.png',
+  ogImage: 'https://juanromero-dotcom.github.io/leo-portafolio/images/Leo.jpeg',
 } as const
