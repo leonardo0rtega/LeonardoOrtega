202e237d1c16b7cc1213f2cb232ac18b0c5582a9
leonardo0rtega <al222410888@gmail.com>
2026-08-16 22:54:27 -0600

    imagenes con url absoluta

--- src/config/site.ts
+++ src/config/site.ts
@@ -2,7 +2,7 @@ export const site = {
   url: 'https://juanromero-dotcom.github.io/leo-portafolio/',
   name: 'Leonardo Emmauel Ortega Nava',
   tagline: 'Marketing Strategy & Project Management',
-  photo: 'images/Leo.jpeg',
-  cv: 'cv/CV-1.png',
-  ogImage: 'images/Leo.jpeg',
+  photo: 'https://juanromero-dotcom.github.io/leo-portafolio/images/Leo.jpeg',
+  cv: 'https://juanromero-dotcom.github.io/leo-portafolio/cv/CV-1.png',
+  ogImage: 'https://juanromero-dotcom.github.io/leo-portafolio/images/Leo.jpeg',
 } as const
